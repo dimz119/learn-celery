6a1dfb5b6e036ad0c228d2048f573cd97aadadae
Seungjoon Lee <joonlee0228@gmail.com>
2024-02-18 22:09:08 -0800

    task signal

--- django_celery/app/worker/tasks.py
+++ django_celery/app/worker/tasks.py
@@ -1,5 +1,6 @@
 import time
 from celery import shared_task
+from celery.signals import task_prerun, task_postrun
 
 @shared_task(queue='celery')
 def print_result(x, y, msg=None):
@@ -52,3 +53,21 @@ def async_task():
     result = sleep_task.apply_async() # type: ignore
     print("Not waiting ...")
     print(result.task_id)
+
+# Define signal handlers
+@task_prerun.connect(sender=add)
+def task_prerun_handler(sender, task_id, task, args, kwargs, **kwargs_extra):
+    print(f"Task {task_id} is about to run: {task.name} with args {args}")
+
+@task_postrun.connect(sender=add)
+def task_postrun_handler(sender, task_id, task, args, kwargs, retval, state, **kwargs_extra):
+    print(f"Task {task_id} has completed: {task.name} with result {retval}")
+
+# simulating task signal
+def simulating_task_signal():
+    # Call the Celery task asynchronously
+    result = add.delay(2, 3) # type: ignore
+
+    # Get the result of the task
+    final_result = result.get()
+    print("Final Result:", final_result)
